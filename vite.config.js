d6c0af0f4af2a4685272a146447f55931a934080
Gopichand251 <sgopichand12@gmail.com>
2026-06-16 20:24:03 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-﻿import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+﻿@'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/referral-dashboard/'
+  plugins: [react()]
 })
+'@ | Set-Content vite.config.js -Encoding utf8
